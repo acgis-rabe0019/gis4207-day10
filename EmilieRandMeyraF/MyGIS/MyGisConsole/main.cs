c74c36f7eae6c0f5fa2f5ec73b0e883e90a1dea1
MDMFR-LAPTOP\Meyra <fuen0023@algonquinlive.com>
2018-03-26 19:04:36 -0400

    returns layer index and name, missing map name

---
+++ EmilieRandMeyraF/MyGIS/MyGisConsole/main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGISBLL;
+
+namespace MyGisConsole
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+
+            Map mapCanada = new Map();
+            mapCanada.Name = "Canada";
+            Layer layerOntario = new Layer();
+            layerOntario.Name = "Ontario";
+           
+            Layer layerManitoba = new Layer();
+            layerManitoba.Name = "Manitoba";
+            Layer layerAlberta = new Layer();
+            layerAlberta.Name = "Alberta";
+
+            mapCanada.AddLayer(layerOntario);
+            mapCanada.AddLayer(layerManitoba);
+            mapCanada.AddLayer(layerAlberta);
+
+            mapCanada.GetLayer(1);
+            Console.WriteLine("", layerManitoba);
+            mapCanada.GetLayerByName("Manitoba");
+            Console.WriteLine("",layerManitoba);
+
+            mapCanada.GetLayer(0);
+            Console.WriteLine("", layerOntario);
+            mapCanada.GetLayerByName("Ontario");
+            Console.WriteLine("", layerOntario);
+
+            mapCanada.GetLayer(1);
+            Console.WriteLine("", layerManitoba);
+            mapCanada.GetLayerByName("Manitoba");
+            Console.WriteLine("", layerManitoba);
+
+            mapCanada.GetLayer(2);
+            Console.WriteLine("", layerAlberta);
+            mapCanada.GetLayerByName("Alberta");
+            Console.WriteLine("", layerAlberta);
+
+
+
+
+
+        }
+       
+    }
+}
